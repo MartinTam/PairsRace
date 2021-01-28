617b76803f5d2fc570dc6e88223a12ffac728423
MartinTam <hongvtam@protonmail.com>
2021-01-28 15:28:55 +0100

    Add success and fail buttons

--- pexeso.py
+++ pexeso.py
@@ -25,15 +25,30 @@ PLAY = pygame.transform.scale( PLAY_ORG, (100, 110) )
 FONT = pygame.font.SysFont('comicsans', 70)
 TEXT_PLAY = FONT.render('PLAY', True, WHITE)
 
+NEXT_ORG = pygame.image.load(os.path.join('images', 'next.png'))
+NEXT = pygame.transform.scale(NEXT_ORG, (200, 200))
+
+SUCCESS_ORG = pygame.image.load(os.path.join('images', 'success.png'))
+SUCCESS = pygame.transform.scale(SUCCESS_ORG, (80,80))
+
+FAIL_ORG = pygame.image.load(os.path.join('images', 'fail.png'))
+FAIL = pygame.transform.scale(FAIL_ORG, (80,80))
+
+
+
 def draw():
     
     WIN.fill(WHITE)
     WIN.blit(BG, (0,0))
-    
+    '''
     WIN.blit(TEXT_PLAY, (430,230))
     WIN.blit(PLAY, (445,295))
     
-    '''
+    WIN.blit(NEXT, (775,525))
+
+    WIN.blit(SUCCESS, (460,23))
+    WIN.blit(FAIL, (460,23))
+
     WIN.blit(IMG1, (270, 135))
     WIN.blit(IMG2, (520, 135))
 
@@ -43,6 +58,9 @@ def draw():
     WIN.blit(IMG3, (270, 481))
     WIN.blit(IMG1, (520, 481))
     '''
+
+
+
     pygame.display.update()
 
 def main():
