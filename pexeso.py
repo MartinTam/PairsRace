1e806511099db8c94cedb344fd5a1226664a9064
MartinTam <hongvtam@protonmail.com>
2021-01-28 14:56:23 +0100

    Add play button

--- pexeso.py
+++ pexeso.py
@@ -19,11 +19,21 @@ IMG1 = pygame.image.load(os.path.join('images', '01.jpeg'))
 IMG2 = pygame.image.load(os.path.join('images', '02.jpeg'))
 IMG3 = pygame.image.load(os.path.join('images', '03.jpeg'))
 
+PLAY_ORG = pygame.image.load(os.path.join('images', 'play.png'))
+PLAY = pygame.transform.scale( PLAY_ORG, (100, 110) )
+
+FONT = pygame.font.SysFont('comicsans', 70)
+TEXT_PLAY = FONT.render('PLAY', True, WHITE)
+
 def draw():
     
     WIN.fill(WHITE)
     WIN.blit(BG, (0,0))
     
+    WIN.blit(TEXT_PLAY, (430,230))
+    WIN.blit(PLAY, (445,295))
+    
+    '''
     WIN.blit(IMG1, (270, 135))
     WIN.blit(IMG2, (520, 135))
 
@@ -32,7 +42,7 @@ def draw():
 
     WIN.blit(IMG3, (270, 481))
     WIN.blit(IMG1, (520, 481))
-    
+    '''
     pygame.display.update()
 
 def main():
